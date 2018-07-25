2a5dc1a51f98afa28b81158b7a8ee9f0e961c68f
Andrii Georgica <georgica.andrii@gmail.com>
2018-07-25 17:47:16 +0300

    Finished task

--- js/test_1.js
+++ js/test_1.js
@@ -1,87 +1,132 @@
-// var amount = 99.99;
 
-// amount = amount*2 ;
-// console.log(amount);
+var point = ".";
+var n = true;
 
-// amount = "$ " + String(amount);
+var str = prompt("Give a text for the program!");
+// var str = "Hello, my name. is Andrii. I'm lookin for new phone.";
 
-// console.log(amount);
+var pontIndex = str.indexOf(point);
+var pointCount = 0;
 
+var str_arr = str.split(" "); // For the first two methods
+// var str_arr = str.split(""); // For the third and fourth method
 
-var a = ".";
-var b = true;
-// var str = prompt("Give a text for the program!");
-var str = "Hello, my name is Andrii. I'm lookin for new phone.";
 var new_str = "";
-var str_arr = str.split(" ");
 
 
-for (i = 0; i<str_arr.length; i++) {
 
-    var i_arr = str_arr[i];
+// The first option
 
-    new_str = new_str.concat(str_arr[i] + " ");
 
-    if (b) {
+// if (pontIndex != -1) {
 
+//     for (i = 0; i<str_arr.length; i++) {
+
+//         var i_arr = str_arr[i];
+    
+//         new_str = new_str.concat(i_arr + " ");
+    
+//         if (n) {
+    
+//             for(j = 0; j<i_arr.length; j++) {
+    
+//                 if (i_arr[j] == point) {
+//                     console.log(str);
+//                     console.log(new_str);
+//                     console.log("here is a point");
+//                     console.log("----------------------------------------------");
+//                     console.log("number of spaces to the point: " + i);
+//                     n = false;
+//                 } 
+        
+//             }
+    
+//         }
+    
+//     }
+    
+//     console.log("----------------------------------------------");
+//     console.log("total number of spaces: " + (str_arr.length - 1));
+//     console.log("----------------------------------------------");
+
+// } else {
+//     console.log("There is no point in this string");
+// }
+
+
+
+// The second option
+
+if (pontIndex != -1) {
+
+    console.log(str);
+    console.log("----------------------------------------------");
+
+    for (i = 0; i<str_arr.length; i++) {
+
+        var i_arr = str_arr[i];        
+    
         for(j = 0; j<i_arr.length; j++) {
-
-            if (i_arr[j] == a) {
-                console.log(new_str);
+    
+            if (i_arr[j] == point) {
+                console.log(new_str.concat(i_arr + " "));
                 console.log("here is a point");
-                console.log("----------------------------------------------");
                 console.log("number of spaces to the point: " + i);
-                b = false;
+                console.log("----------------------------------------------");
             } 
     
         }
-
-    } else {
-        break;
+    
     }
+    
+    console.log("total number of spaces: " + (str_arr.length - 1));
+    console.log("----------------------------------------------");
 
-
+} else {
+    console.log("There is no point in this string");
 }
 
-console.log("----------------------------------------------");
-console.log("total number of spaces: " + (str_arr.length - 1));
-console.log("----------------------------------------------");
-
-for (i = 0; i<str_arr.length; i++) {
-
-    var i_arr = str_arr[i];
-
-    for(j = 0; j<i_arr.length; j++) {
-
-        console.log(i_arr);
-
-        if (i_arr[j] == a) {
-            if (j == str.indexOf(".")) {
-                console.log("Here is the first point");
-            } else {
-                console.log("here is a point");
-            }
-            
-        } 
-
-    }
-
-}
-
-console.log("----------------------------------------------");
-console.log("total number of spaces: " + (str_arr.length - 1));
-console.log("----------------------------------------------");
 
 
-// var str = "Hello, my name is Andrii. I'm lookin for new phone.";
-// var new_str = "";
-// var str_arr = str.split(" ");
-// var point = str.indexOf(".");
+// The third option
 
-// for(i = 0; i<str.length; i++) {
+// if (pontIndex != -1) {
 
-//     new_str = new_str.concat(str[i]);
+//     for (i = 0; i < (pontIndex + 1); i++) {
 
+//         var i_arr = str_arr[i];
+//         new_str = new_str.concat(i_arr);
+//     }
+
+//     console.log(new_str);
+//     console.log("Here is the first point");
+
+// } else {
+//     console.log("There is no point in this string");
 // }
 
-// console.log(new_str);
+
+
+// The fourth option
+
+// if (pontIndex != -1) {
+
+//     console.log(str);
+//     console.log("----------------------------------------------");
+
+//     for (i = 0; i < str_arr.length; i++) {
+
+//         var i_arr = str_arr[i];
+        
+//         if (str[i] == point) {
+
+//             pointCount = pointCount + 1;
+
+//         }
+//     }
+
+//     console.log("There is " + pointCount + " points in this string");
+
+// } else {
+//     console.log("There is no point in this string");
+// }
