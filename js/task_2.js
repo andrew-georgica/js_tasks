afcb10b4cd4a40f2ec475c5d5a78f68456a85489
Andrii Georgica <georgica.andrii@gmail.com>
2018-08-06 15:38:21 +0300

    The first four tasks

--- js/task_2.js
+++ js/task_2.js
@@ -1,72 +1,11 @@
 
-var user_arr = prompt("Enter an array");
-var arr = user_arr.split(" ");
-// var arr = [1, 4, 3];
+var mass = prompt("Please enter your weight in kilograms");
+// var mass = 70;
 
-var num = prompt("Enter the number of the array element");
-
-
-function check_arr() {
-    
-    for (i = 0; i < user_arr.length; i++) {
-        if (isNaN(user_arr[i])) {
-            // console.log("Please enter an array of numbers");
-            return true;
-        }
-    }
-
-    return false;
+if(isNaN(mass)) {
+    console.log("Please enter your weight only by number");
+} else {
+    console.log("Your weight in the month is " + mass*0.17 + "kg");
 }
-
-
-function arr_element(num) {
-    
-    // console.log(arr);
-
-    if(num == 0) {
-        num = 1;
-    }
-
-    num = num - 1;
-    // console.log(num);
-
-    if (check_arr()) {
-        console.log("Please enter an array of numbers");
-    }else if (isNaN(num)) {
-        console.log("Please enter a number");
-    }else if (num > arr.length) {
-        console.log("This number goes beyond the array. Enter a smaller number or a larger array.");
-    }else if(num < 0) {
-        console.log("The element of the array can not be less than zero");
-    }
-     else {
-
-        for (i = 0; i < arr.length; i++) {
-
-            if(i == num) {
-                console.log("Entered array");
-                console.log(arr);
-
-                arr[i] = Number(arr[i])*0.1 + Number(arr[i]);
-                // console.log(typeof arr[i]);
-                // console.log(isNaN(arr[i]));
-                console.log(arr[i]);
-
-                console.log("Array after increasing the " + i + " element by 10%");
-                console.log(arr);
-                
-            }
-            
-        }
-
-    }
-
-    
-
-}
-
-arr_element(num);
-
-
 
 
